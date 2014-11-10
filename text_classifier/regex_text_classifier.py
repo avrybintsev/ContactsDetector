a1e24f849b71681d64055615e734afec38e35084
avrybintsev <avrybintsev@gmail.com>
2014-11-10 08:31:41 +0300

    [py-text] fix classifiers

--- text_classifier/regex_text_classifier.py
+++ text_classifier/regex_text_classifier.py
@@ -2,26 +2,28 @@
 
 import re
 
+from text_classifier import TextClassifier
+
 
 class RegexTextClassifier(TextClassifier):
-	def classify(self, text):
-		def preprocess_text(text):
-			return text.replace(' ', '')
+    def classify(self, text):
+        def preprocess_text(text):
+            return text.replace(' ', '')
 
-		regex = [
-    		re.compile(r'9\d\d'),
-    		re.compile(r'\+7'),
-    		re.compile(r'@'),
-    		re.compile(r'\d\d-\d\d-\d\d'),
-    		re.compile(r'ww'),
-    		re.compile(r'\.\w\w'),
-    		re.compile(r'\(\d\d\d\)'),
-    		re.compile(r'http'),
-    	]
+        regex = [
+            re.compile(r'9\d\d'),
+            re.compile(r'\+7'),
+            re.compile(r'@'),
+            re.compile(r'\d\d-\d\d-\d\d'),
+            re.compile(r'ww'),
+            re.compile(r'\.\w\w'),
+            re.compile(r'\(\d\d\d\)'),
+            re.compile(r'http'),
+        ]
 
-    	_text = preprocess_text(text)
-	    for patern in regex.items():
-        	if pattern.match(_text)
-            	return self.target_class
+        _text = preprocess_text(text)
+        for pattern in regex:
+            if pattern.match(_text):
+                return self.target_class
         return self.second_class
-		
+        
--- text_classifier/simple_text_classifier.py
+++ text_classifier/simple_text_classifier.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 
+from text_classifier import TextClassifier
+
+
 class SimpleTextClassifier(TextClassifier):
-	def classify(self, text):
-		_parts = [
-			'@', 
-			'.ru', 'ww', '.com', '.ua',
-			'+7', '925', '926', '921', '918', '916', '903', # ...
-		]
-		for part in _parts:
-			if part in text:
-				return self.target_class
-		return self.second_class
-		
+    def classify(self, text):
+        _parts = [
+            '@', 
+            '.ru', 'ww', '.com', '.ua',
+            '+7', '925', '926', '921', '918', '916', '903', # ...
+        ]
+        for part in _parts:
+            if part in text:
+                return self.target_class
+        return self.second_class
+        
