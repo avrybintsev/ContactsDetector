224f5fb17ecd8705ade6889347d00ca685724547
avrybintsev <avrybintsev@gmail.com>
2014-11-10 13:14:06 +0300

    [py] add report

--- stats.py
+++ stats.py
@@ -17,5 +17,14 @@ class Stats:
 		self.err1t = len(filter(err1t_criteria, results))
 		self.err2t = len(filter(err2t_criteria, results))
 
+		tp = len(filter(lambda pair: pair.answer == 'with' == pair.correct, results))
+		fp = self.err1t
+		tn = len(filter(lambda pair: pair.answer == 'without' == pair.correct, results))
+		fn = self.err2t
+
+		self.recall = (tp + 0.0) / (tp + fn) if tp + fn else 0
+		self.precision = (tp + 0.0) / (tp + fp) if tp + fp else 0
+		self.f1 = (2.0 * self.precision * self.recall) / (self.precision + self.recall) if self.precision + self.recall else 0
+
 	def __repr__(self):
 		return 'Total: %d, Correct: %d, Errors-I: %d, Errors-II: %d' % (self.total, self.correct, self.err1t, self.err2t)
--- test.py
+++ test.py
@@ -7,7 +7,8 @@ from collections import namedtuple
 from text_classifier.regex_text_classifier import RegexTextClassifier
 from file_classifier import classify_file
 from base import Base
-from stats import Pair, Stats
+from stats import Pair
+from report import excel_report
 
 
 if __name__ == '__main__':
@@ -16,10 +17,9 @@ if __name__ == '__main__':
 	base = Base({'with': 'images/with', 'without': 'images/without'})
 	regex_text_classifier = RegexTextClassifier(base.get_class_names(), target_class='with')
 	make_pair = lambda filename, correct: Pair(answer=classify_file(filename, regex_text_classifier), correct=correct)
-	results = pool.map(lambda item: make_pair(item['file_name'], item['class_name']), base.iter_dicts())
+	results = pool.map(lambda item: make_pair(item['file_name'], item['class_name']), list(base.iter_dicts())[:50] + list(base.iter_dicts())[::-1][:50])
 
 	pool.close()
 	pool.join()
 
-	print Stats(results)
-
+	excel_report(results)
