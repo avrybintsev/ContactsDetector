34af0abdb5ebbce68a055727ec5b11e889d38a92
avrybintsev <avrybintsev@gmail.com>
2014-11-10 13:11:41 +0300

    [py] add excel report handler

---
+++ report.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import xlwt
+
+from stats import Stats
+
+
+def excel_report(results, file_name='results.xls'):
+	stats = Stats(results)
+
+	wb = xlwt.Workbook()
+	ws = wb.add_sheet('Statistics')
+
+	ws.write(0, 0, 'Contacts detection')
+
+	def write_pairs(pairs, start_row=1):
+		current = start_row
+		for k, v in pairs.iteritems():
+			ws.write(current, 0, k)
+			ws.write(current, 1, v)
+			current += 1
+
+	write_pairs({
+		'Total': stats.total,
+		'Correct': stats.correct,
+		'Type I Errors': stats.err1t,
+		'Type II Errors': stats.err2t,
+		'Precision': stats.precision,
+		'Recall': stats.recall,
+		'F1-score': stats.recall,
+	})
+
+	wb.save(file_name)
